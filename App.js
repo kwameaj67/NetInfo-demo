eb8fac20de95ce956453efd1702143607b75453f
unknown <kwameagyenimboateng64@gmail.com>
2020-01-25 00:55:48 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, Text, View,Dimensions } from 'react-native';
+import  NetInfoState from '@react-native-community/netinfo'
+import * as Animatable from 'react-native-animatable';
+
+const { width } = Dimensions.get('window');
+export default class App extends React.Component {
+  state = {
+    connected:true
+  }
+  componentDidMount(){
+    NetInfoState.isConnected.addEventListener('connectionChange',this.handleConnectivity);
+  }
+  componentWillUnmount(){
+    NetInfoState.isConnected.removeEventListener('connectionChange',this.handleConnectivity);
+  }
+  handleConnectivity = (connected) => {
+    this.setState({connected});
+    console.log(`is connected: ${this.state.connected}`)
+  }
+  
+  render(){
+  return(
+    <View style={styles.container}>
+      {this.state.connected
+      ?
+      <Animatable.View style={styles.showtext} animation="fadeOutUp" delay={3000}>
+      <View style={{backgroundColor: 'green',padding:15,borderRadius:5}}>
+            <Text style={styles.about}>You are connected.</Text>
+         </View>
+       </Animatable.View>
+      :
+      <View style={styles.showtext}>
+         <View style={{backgroundColor: '#b52424',padding:15,borderRadius:5}}>
+            <Text style={styles.about}>Oops! Please check your internet connection</Text>
+         </View>
+       </View>
+      }
+    </View>
+  )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  showtext:{
+      height: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
+      width,
+      position: 'absolute',
+      top: 30,
+    },
+      about:{
+      fontSize: 14,
+      letterSpacing: -0.4,
+      color:"white",
+      },
+});
